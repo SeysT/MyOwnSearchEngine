4ed091b2d7b23841e7edfa13888f4aeca6ca63e2
Yoann Gauthier <yoann.gauthier@nebulae.co>
2017-12-04 11:31:06 +0100

    Adding funtions and structure to clean the CACM collection against the stop words list.

--- document.py
+++ document.py
@@ -1,13 +1,43 @@
+from nltk.tokenize import word_tokenize
+
+
 class Document(object):
 
-    index_fields = ('title', 'summary', 'key_words')
+    tokenized_fields = ('title_tokenized', 'summary_tokenized', 'key_words_tokenized')
+    signs_to_remove = [',', '\'', ';', ':', '.', '?', '!']
 
     def __init__(self, id):
         self.id = id
         self.title = ''
+        self.title_tokenized = []
         self.summary = ''
+        self.summary_tokenized = []
         self.publication_date = ''
         self.authors = ''
         self.add_date = ''
         self.references = ''
         self.key_words = ''
+        self.key_words_tokenized = []
+        self.citations = ''
+
+    def tokenize_doc(self):
+        self.title_tokenized = word_tokenize(self.title)
+        self.summary_tokenized = word_tokenize(self.summary)
+        self.key_words_tokenized = word_tokenize(self.key_words)
+
+    def clean_tokens(self, common_words):
+
+        for token in self.title_tokenized:
+            if token in common_words or token in Document.signs_to_remove:
+                self.title_tokenized.remove(token)
+
+        for token in self.summary_tokenized:
+            if token in common_words or token in Document.signs_to_remove:
+                self.summary_tokenized.remove(token)
+
+        for token in self.key_words_tokenized:
+            if token in common_words or token in Document.signs_to_remove:
+                self.key_words_tokenized.remove(token)
+
+    def __repr__(self):
+        return "DOCID : {}\nTITLE : {}\nSUMMARY : {}\nKEYWORDS : {}".format(self.id, self.title_tokenized, self.summary_tokenized, self.key_words_tokenized)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from document import Document
-
+from nltk.tokenize import word_tokenize
 
 data_filename = 'Data/CACM/cacm.all'
+stop_list_filename = 'Data/CACM/common_words'
 
 fields_mapping = {
     '.T': 'title',
@@ -11,24 +12,61 @@ fields_mapping = {
     '.N': 'add_date',
     '.X': 'references',
     '.K': 'key_words',
+    '.C': 'citations',
 }
 
 document_collection = {}
+common_words = []
 
-with open(data_filename, 'r') as data_filename:
-    data = data_filename.read()
 
-for line in data.split('\n'):
-    if line.startswith('.I'):
-        current_id = line.replace('.I ', '')
-        document_collection[current_id] = Document(current_id)
-    elif line in fields_mapping.keys():
-        attr = fields_mapping[line]
-    else:
-        attr_value = getattr(document_collection[current_id], attr)
-        to_add = line.strip()
-        setattr(
-            document_collection[current_id],
-            attr,
-            '{}\n{}'.format(attr_value, to_add) if attr_value else to_add
-        )
+def populate_document_collection():
+    with open(data_filename, 'r') as df:
+        data = df.read()
+
+    for line in data.split('\n'):
+        if line.startswith('.I'):
+            current_id = line.replace('.I ', '')
+            document_collection[current_id] = Document(current_id)
+        elif line in fields_mapping.keys():
+            attr = fields_mapping[line]
+        else:
+            attr_value = getattr(document_collection[current_id], attr)
+            to_add = line.strip()
+            setattr(
+                document_collection[current_id],
+                attr,
+                '{}\n{}'.format(attr_value, to_add) if attr_value else to_add
+            )
+    return document_collection
+
+
+def populate_common_words_set():
+    with open(stop_list_filename, 'r') as sl:
+        data = sl.read()
+
+    for line in data.split('\n'):
+        common_words.append(line.strip())
+
+    return common_words
+
+
+def tokenize_collection(document_collection):
+    for el in document_collection.values():
+        el.tokenize_doc()
+
+
+def clean_document_collection(document_collection, common_words):
+    for el in document_collection.values():
+        el.clean_tokens(common_words)
+
+
+if __name__ == '__main__':
+    populate_document_collection()
+    populate_common_words_set()
+    test = document_collection['1765']
+
+    tokenize_collection(document_collection)
+    print(test)
+    clean_document_collection(document_collection, common_words)
+
+    print(test)
