d25cfc8f5969672a19193da002a152950f202daa
SeysT <thibaut.seys@hotmail.fr>
2017-12-18 00:28:48 +0100

    add install scripts

---
+++ build.py
@@ -0,0 +1,15 @@
+from models.document import CACMDocumentCollection
+from models.reverse_index import ReverseIndex
+
+
+if __name__ == '__main__':
+    cacm_document_collection = CACMDocumentCollection(
+        data_filename='Data/CACM/cacm.all',
+        stop_list_filename='Data/CACM/common_words',
+        load_on_creation=True
+    )
+
+    cacm_reverse_index = ReverseIndex(document_collection=cacm_document_collection)
+
+    cacm_document_collection.save('Data/Collection/cacm.collection')
+    cacm_reverse_index.save('Data/Index/cacm.index')
